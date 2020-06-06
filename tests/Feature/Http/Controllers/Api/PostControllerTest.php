56dd96b6a96d5c9b2a812b0dda5d1f82738d041e
doug9507 <dpbarbaran7@gmail.com>
2020-06-05 21:04:28 -0500

    Creacion del metodo de la prueba test_store, configurando los assert y uso de la clase RefreshDatabase

--- tests/Feature/Http/Controllers/Api/PostControllerTest.php
+++ tests/Feature/Http/Controllers/Api/PostControllerTest.php
@@ -8,15 +8,25 @@ use Tests\TestCase;
 
 class PostControllerTest extends TestCase
 {
-    /**
-     * A basic feature test example.
-     *
-     * @return void
-     */
-    public function testExample()
-    {
-        $response = $this->get('/');
+    // public function testExample()
+    // {
+    //     $response = $this->get('/');
 
-        $response->assertStatus(200);
+    //     $response->assertStatus(200);
+    // }
+    use RefreshDatabase;
+
+    public function test_store()
+    {
+        $this->withoutExceptionHandling();
+        $response = $this->json('POST','/api/posts',[
+            'title' => 'Post de prueba para el TDD.'
+        ]);
+
+        $response->assertJsonStructure(['id','title','created_at','updated_at'])
+        ->assertJson(['title' => 'Post de prueba para el TDD.'])
+        ->assertStatus(201);
+
+        $this->assertDatabaseHas('posts',['title' => 'Post de prueba para el TDD.']);
     }
 }
